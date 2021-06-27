f88cc3d144ce1b2e6682ffc6bf10f64e419e37f2
Jonathan E. Magen <yonkeltron@gmail.com>
2021-06-27 14:33:46 -0400

    initial commit
    Signed-off-by: Jonathan E. Magen <yonkeltron@gmail.com>

---
+++ pages/games/_id.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Game: {{ id }}</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      id: this.$route.params.id,
+    }
+  },
+})
+</script>
